defb30057b7b99c30360090cc9129d79181d503c
mihirgupta665 <mihirgupta665@gmail.com>
2025-08-17 20:40:13 +0530

    Finishing Intro of CPP2

---
+++ 1_intro_n_basic/7_input_Output.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cin>>a;
+    int b;
+    cin>>b;
+    int c=a+b;
+    cout<<"The sum of "<<a<<" and "<<b<<" is : "<<c<<endl;
+    return 0;
+}
