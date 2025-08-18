0a109d3682a6457e257dad4c6cb76c8414a15f6b
mihirgupta665 <mihirgupta665@gmail.com>
2025-08-18 10:28:11 +0530

    nested if else

---
+++ 2_condition_n _loop_basics/1_nested_if_else.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int amt;
+    cin>>amt;
+    if(amt>5000){
+        if(amt>10000){
+            cout<<"I";
+        }
+        else{
+            cout<<"am";
+        }
+    }
+    else if(amt>2000){
+        cout<<"mihir";
+    }
+    else{
+        cout<<"gupta";
+    }   
+    }
