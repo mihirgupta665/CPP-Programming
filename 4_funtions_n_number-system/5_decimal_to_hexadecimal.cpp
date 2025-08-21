b4d927c95092021a38bab573b227d82d61f452c8
mihirgupta665 <mihirgupta665@gmail.com>
2025-08-21 09:44:25 +0530

    Decimal to Hexadecimal

---
+++ 4_funtions_n_number-system/5_decimal_to_hexadecimal.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int x=1;
+    string ans="";
+    while(x<=n){
+        x*=16;
+    }
+    x/=16;
+    while(x>0){
+        int ld=n/x;
+        n-=ld*x;
+        x/=16;
+        if(ld<=9){
+            ans=ans+ to_string(ld);
+        }
+        else{
+            char c= 'A'+ld-10;
+            ans.push_back(c);
+        }
+    }
+    cout<<ans<<endl;
+    return 0;
+}
+
+
