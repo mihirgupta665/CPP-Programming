4c73e5334d064892dceede584b7e597937015f26
mihirgupta665 <mihirgupta665@gmail.com>
2025-08-28 10:35:55 +0530

    pairing problem

---
+++ 7_Rec/12_pairing_problem.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int pairing(int n){
+    if(n==0 || n==1 || n==2){
+        return n;
+    }
+    return pairing(n-1)+pairing(n-2) * (n-1);
+}
+int main(){
+    int n;
+    cout<<"Enter the number of friends :";
+    cin>>n;
+    cout<<pairing(n)<<endl;
+}
