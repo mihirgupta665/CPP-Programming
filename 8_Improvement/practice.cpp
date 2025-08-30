9ae8146dd4d533c0b6706e89b7b34a407652de9f
mihirgupta665 <mihirgupta665@gmail.com>
2025-08-30 23:39:07 +0530

    Improvements

---
+++ 8_Improvement/practice.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+#include<cctype>
+using namespace std;
+int main(){
+    char c= 'A' +5;     // char me int plus kar ke agar char me store kare to char value he update hoti hai
+    cout<<c<<endl;
+    int a=5,b=7;
+    cout<<to_string(a)<<endl;
+    // cout<<c.push_back('d');
+    int b=67793;
+    return 0;
+}
