b0a45117cdc487051869d5a897f407f6f3c5ad5f
mihirgupta665 <mihirgupta665@gmail.com>
2025-09-03 21:53:58 +0530

    fstream iso app

---
+++ 10_Neo_Colab_Practice/unit-3_File-Handling/4_fstream_iso-app.cpp
@@ -0,0 +1,29 @@
+Problem Statement
+
+
+
+Ragul wants to write a program that performs a specific operation on a binary tree. The program should take an array of elements as input and construct a binary tree. Once the tree is constructed, the program should reverse the alternate levels of the binary tree. Finally, it should print the in-order traversal of the modified tree.
+
+Input format :
+The first line will contain an integer n, representing the number of elements in the array.
+
+The second line will contain n space-separated integers, representing the elements of the array.
+
+Output format :
+The output will be a single line containing the elements of the binary tree after reversing the alternate levels, displayed in the in-order traversal. The elements should be separated by spaces.
+
+
+
+Refer to the sample output for the formatting specifications.
+
+Code constraints :
+1 <= n <= 10
+
+1 <= array elements <= 1000
+
+Sample test cases :
+Input 1 :
+4 
+1 2 3 4
+Output 1 :
+4 3 1 2 
